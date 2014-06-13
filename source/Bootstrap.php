98c6e0d6cf401af458682266f690714fa2074095
Manuel Kiessling <manuel@kiessling.net>
2014-06-13 13:10:11 +0200

    Added PHP Code Sniffer setup and fixed files accordingly

--- source/Bootstrap.php
+++ source/Bootstrap.php
@@ -4,8 +4,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/api/locations', function () {
-    return 'Hello World';
-});
+$app->get(
+    '/api/locations',
+    function () {
+        return 'Hello World';
+    }
+);
 
 return $app;
