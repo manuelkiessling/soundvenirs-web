bdf5442e5984194696333578af30eeabe6f30793
Manuel Kiessling <manuel@kiessling.net>
2014-06-14 22:28:05 +0200

    Refactored common test setup code into our own SoundvenirsWebTestCase class

--- tests/functional/api/SoundLocationsTest.php
+++ tests/functional/api/SoundLocationsTest.php
@@ -1,48 +1,24 @@
 <?php
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../SoundvenirsWebTestCase.php';
 
-use Silex\WebTestCase;
-
-class ApiLocationsTest extends WebTestCase
+class ApiLocationsTest extends SoundvenirsWebTestCase
 {
-    public $app;
-
-    public function createApplication()
-    {
-        $app = require __DIR__ . '/../../../source/Bootstrap.php';
-        $app['debug'] = true;
-        $app['exception_handler']->disable();
-        $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-            'db.options' => array(
-                'driver' => 'pdo_sqlite',
-                'path' => '/var/tmp/soundvenirs.test.sqlite',
-            ),
-        ));
-        $this->app = $app;
-        return $app;
-    }
-
-    protected function resetDatabase()
-    {
-        $this->app['db']->query('DROP TABLE IF EXISTS sounds;');
-        $this->app['db']->query(
-            'CREATE TABLE sounds(
-              uuid CHAR(36) PRIMARY KEY NOT NULL,
-              title TEXT NOT NULL,
-              lat FLOAT NULL,
-              long FLOAT NULL,
-              mp3url TEXT
-            );'
-        );
-    }
-
     public function testGetAllSoundLocations()
     {
         $this->resetDatabase();
-        $this->app['db']->query('INSERT INTO sounds (uuid, title, lat, long, mp3url) VALUES ("1", "First Song", 11.1, 1.11, "http://foo/bar");');
-        $this->app['db']->query('INSERT INTO sounds (uuid, title, lat, long, mp3url) VALUES ("2", "Second Song", 22.2, 2.22, "http://foo/bar");');
-        $this->app['db']->query('INSERT INTO sounds (uuid, title, lat, long, mp3url) VALUES ("3", "Third Song", NULL, NULL, "http://foo/bar");');
+        $this->app['db']->query(
+            'INSERT INTO sounds (uuid, title, lat, long, mp3url)
+            VALUES ("1", "First Song", 11.1, 1.11, "http://foo/bar");'
+        );
+        $this->app['db']->query(
+            'INSERT INTO sounds (uuid, title, lat, long, mp3url)
+            VALUES ("2", "Second Song", 22.2, 2.22, "http://foo/bar");'
+        );
+        $this->app['db']->query(
+            'INSERT INTO sounds (uuid, title, lat, long, mp3url)
+            VALUES ("3", "Third Song", NULL, NULL, "http://foo/bar");'
+        );
         $client = $this->createClient();
         $client->request('GET', '/api/soundLocations');
         $content = $client->getResponse()->getContent();
