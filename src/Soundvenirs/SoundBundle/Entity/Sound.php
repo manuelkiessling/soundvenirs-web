9d5703e1ac9cc59ba9019b8993cd4f25f3acf1ab
Manuel Kiessling <manuel@kiessling.net>
2014-06-24 18:21:03 +0200

    Added Doctrine ORM mappings to Sound entity

--- src/Soundvenirs/SoundBundle/Entity/Sound.php
+++ src/Soundvenirs/SoundBundle/Entity/Sound.php
@@ -2,10 +2,31 @@
 
 namespace Soundvenirs\SoundBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
 class Sound
 {
+    /**
+     * @ORM\Column(type="string", length=6)
+     * @ORM\Id
+     */
     public $id;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
     public $title;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
     public $lat;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
     public $long;
 }
