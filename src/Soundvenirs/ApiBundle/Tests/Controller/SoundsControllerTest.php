c4fa52a4b9a4225d802987ae9127564c3ba3a2a5
Manuel Kiessling <manuel@kiessling.net>
2014-06-24 21:37:02 +0200

    Refactored test case, renamed according to route that is tested, added test for creating a sound route

--- src/Soundvenirs/ApiBundle/Tests/Controller/SoundsControllerTest.php
+++ src/Soundvenirs/ApiBundle/Tests/Controller/SoundsControllerTest.php
@@ -7,24 +7,32 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiSoundsTest extends WebTestCase
 {
-    public function testGetOneSound()
-    {
-        $client = static::createClient();
-        $container = $client->getContainer();
-        $entityManager = $container->get('doctrine.orm.entity_manager');
+    protected $client;
+    protected $entityManager;
 
-        $connection = $entityManager->getConnection();
-        $platform   = $connection->getDatabasePlatform();
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $container = $this->client->getContainer();
+        $this->entityManager = $container->get('doctrine.orm.entity_manager');
+
+        $connection = $this->entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform();
 
         $connection->executeUpdate($platform->getTruncateTableSQL('Sound', true));
+    }
+
+    public function testRetrieveOneSound()
+    {
+        $client = static::createClient();
 
         $sound = new Sound();
         $sound->id = 'ab12cd';
         $sound->title = 'First Song';
         $sound->lat = 11.1;
         $sound->long = 1.11;
-        $entityManager->persist($sound);
-        $entityManager->flush();
+        $this->entityManager->persist($sound);
+        $this->entityManager->flush();
 
         $client->request('GET', '/api/sounds/ab12cd');
         $content = $client->getResponse()->getContent();
@@ -32,5 +40,13 @@ class ApiSoundsTest extends WebTestCase
             '{"id":"ab12cd","title":"First Song","mp3url":"http:\/\/www.soundvenirs.com\/download\/ab12cd.mp3","location":{"lat":11.1,"long":1.11}}',
             $content
         );
+    }
+
+    public function testCreateSound()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/sounds', array(), array(), array('CONTENT_TYPE' => 'application/json'), '{"title":"First Song"}');
+        $content = $client->getResponse()->getContent();
+        $this->assertRegExp('/^\{"id":"[0-9a-z]{1,6}"\}$/', $content);
     }
 }
