17ce1685bf283b3a1201b52d316b20627a7792f3
Manuel Kiessling <manuel@kiessling.net>
2014-06-25 19:24:27 +0200

    Added testcase for sound file upload

--- src/Soundvenirs/HomepageBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Soundvenirs/HomepageBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function setUp()
     {
         $client = static::createClient();
+        $container = $client->getContainer();
+        $entityManager = $container->get('doctrine.orm.entity_manager');
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform();
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $connection->executeUpdate($platform->getTruncateTableSQL('Sound', true));
+    }
+
+    public function testUpload()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $uploadForm = $crawler->selectButton('Upload')->form();
+        $uploadForm['form[soundfile]']->upload(__DIR__.'/../assets/soundfile.mp3');
+        $uploadForm['form[title]'] = 'First Song';
+        $client->submit($uploadForm);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $repo = $client->getContainer()->get('soundvenirs_domain.sound_repository');
+        $sounds = $repo->findAll();
+        $sound = $sounds[0];
+
+        $this->assertEquals('First Song', $sound->title);
+        $this->assertTrue(file_exists('/var/tmp/soundvenirs-'.$sound->id.'.mp3'));
+        unlink('/var/tmp/soundvenirs-'.$sound->id.'.mp3');
     }
 }
