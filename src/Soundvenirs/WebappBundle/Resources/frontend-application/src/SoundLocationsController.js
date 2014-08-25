33829eb5ad7a6fd71b21d17c473780e9b9a76a04
Manuel Kiessling <manuel@kiessling.net>
2014-08-25 11:13:45 +0200

    Map is now fullscreen, labels look good

--- src/Soundvenirs/WebappBundle/Resources/frontend-application/src/SoundLocationsController.js
+++ src/Soundvenirs/WebappBundle/Resources/frontend-application/src/SoundLocationsController.js
@@ -7,13 +7,16 @@ angular.module('soundvenirsWebapp')
     .controller('SoundLocationsController', function ($scope, SoundLocationsService) {
         $scope.map = {
             center: {
-                latitude: 45,
-                longitude: -73
+                latitude: 45.000,
+                longitude: -73.000
             },
             zoom: 8
         };
 
         SoundLocationsService.getAll().then(function(soundLocations) {
+            for (var i = 0; i < soundLocations.length; i++) {
+                soundLocations[i].randomId = Math.random();
+            }
             $scope.soundLocations = soundLocations;
         });
     });
